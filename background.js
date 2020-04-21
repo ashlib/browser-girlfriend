f003549f0320ff73409b357abddbb6dcb78c7ba5
thegiggler <58568758+ashlib@users.noreply.github.com>
2020-04-21 08:45:06 -0400

    final commit

--- background.js
+++ background.js
@@ -7,12 +7,30 @@
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
   console.log('the user typed "' + text + '"');
 
-  suggest([
-    { content: 'depressed', description: 'depressed? sad?' },
-    { content: 'bored', description: 'bored?' },
-    { content: 'fine', description: 'fine? okay?' },
-    { content: 'happy', description: 'happy?' },
-    { content: 'missing me', description: 'missing me?' },
+  suggest([{
+      content: 'text',
+      description: 'SUGGESTIONS/OPTIONS BELOW'
+    },
+    {
+      content: 'sad',
+      description: 'sad? depressed? hurt?'
+    },
+    {
+      content: 'bored',
+      description: 'bored'
+    },
+    {
+      content: 'fine',
+      description: 'fine? good? okay?'
+    },
+    {
+      content: 'happy',
+      description: 'great? happy?'
+    },
+    {
+      content: 'missing me',
+      description: 'missing me? *write "you" not "me"*'
+    },
   ]);
 });
 
@@ -20,62 +38,180 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
   console.log('the user entered: "' + text + '"');
   chrome.tabs.executeScript({
 
-    code:
-    `
-    var images = [
-    "images/myAvatarHugs.png",
-    "images/myAvatarLaugh.png",
-    "images/myAvatarSmile.png",
-    "images/myAvatarYay.png",
-    "images/myAvatarCall.png",
-    "images/myAvatarSleep.png",
+    code: `
+
+    var avatar = [
+    "images/avatars/myAvatarHugs.png",
+    "images/avatars/myAvatarLaugh1.png",
+    "images/avatars/myAvatarSmile.png",
+    "images/avatars/myAvatarYay.png",
+    "images/avatars/myAvatarCall.png",
+    "images/avatars/myAvatarSleep.png",
     ];
 
-    if ("${text}" == "sad" || "${text}" == "depressed") {
+    var insta = [
+    "images/instagram/insta.jpg",
+    "images/instagram/insta1.jpg",
+    "images/instagram/insta2.jpg",
+    "images/instagram/insta3.jpg",
+    "images/instagram/insta4.jpg",
+    "images/instagram/insta5.jpg",
+    "images/instagram/insta6.jpg",
+    "images/instagram/insta7.jpg",
+    "images/instagram/insta8.jpg",
+    "images/instagram/insta9.jpg",
+    ];
+
+    var randomInsta = insta[Math.floor(Math.random() * insta.length)];
+
+    if ("${text}" == "sad" || "${text}" == "depressed" || "${text}" == "hurt") {
       document.getElementById("text").innerHTML = "Oh no! Hugs?";
-      document.getElementById("avatar").src = images[0];
+      document.getElementById("avatar").src = avatar[0];
+
+      document.getElementById("heart-img").style.display = "block";
+      document.getElementById("html").style.backgroundSize = "200px";
+      document.body.style.backgroundSize = "200px";
+      document.getElementById("cheerup-note").style.display = "block";
+
+      document.getElementById("insta").style.display = "none";
+      document.getElementById("cam").style.display = "none";
+      document.getElementById("camButton").style.display = "none";
+      document.getElementById("camPhoto").style.display = "none";
+      document.getElementById("cam-note").style.display = "none";
+      document.getElementById("fireworks").style.display = "none";
+      document.getElementById("drag-note").style.display = "none";
+      document.getElementById("link").style.display = "block";
+
+      scrollToTop();
     }
 
     if ("${text}" == "bored") {
+      document.body.style.marginBottom = "275px";
+
       document.getElementById("text").innerHTML = "No worries, look at all these funny Instagram posts! I DIED!";
-      document.getElementById("avatar").src = images[1];
+      document.getElementById("avatar").src = avatar[1];
+
+      document.getElementById("insta").src = randomInsta;
+      document.getElementById("insta").style.display = "block";
+
+      document.getElementById("heart-img").style.display = "none";
+      document.getElementById("cheerup-note").style.display = "none";
+      document.getElementById("cam").style.display = "none";
+      document.getElementById("camButton").style.display = "none";
+      document.getElementById("camPhoto").style.display = "none";
+      document.getElementById("cam-note").style.display = "none";
+      document.getElementById("fireworks").style.display = "none";
+      document.getElementById("drag-note").style.display = "none";
+      document.getElementById("link").style.display = "block";
+
+      document.getElementById("html").style.backgroundSize = "0px";
+      document.body.style.backgroundSize = "0px";
+
+      scrollToTop();
     }
 
-    if ("${text}" == "fine"  || "${text}" == "okay" ) {
-      document.getElementById("text").innerHTML = "That's not good enough! Let me see you smile!!!!";
-      document.getElementById("avatar").src = images[2];
+
+    if ("${text}" == "fine"  || "${text}" == "good" || "${text}" == "okay") {
+      document.body.style.marginBottom = "10px";
+
+      document.getElementById("text").innerHTML = "That's not good enough! You should be great! Let me see you smile!!!!";
+      document.getElementById("avatar").src = avatar[2];
+
+
+      document.getElementById("cam").style.display = "block";
+      document.getElementById("camButton").style.display = "block";
+      document.getElementById("camPhoto").style.display = "block";
+      document.getElementById("cam-note").style.display = "block";
+
+      document.getElementById("heart-img").style.display = "none";
+      document.getElementById("cheerup-note").style.display = "none";
+      document.getElementById("insta").style.display = "none";
+      document.getElementById("fireworks").style.display = "none";
+      document.getElementById("drag-note").style.display = "none";
+      document.getElementById("link").style.display = "block";
+
+      document.getElementById("html").style.backgroundSize = "0px";
+      document.body.style.backgroundSize = "0px";
+
+      scrollToTop();
+      videoOn();
     }
 
-    if ("${text}" == "happy") {
+    if ("${text}" == "great" || "${text}" == "happy") {
+      // document.body.style.marginBottom = "15px";
+
       document.getElementById("text").innerHTML = "Yay!!!";
-      document.getElementById("avatar").src = images[3];
+      document.getElementById("avatar").src = avatar[3];
+      document.getElementById("avatar").style.transform = "translateY(30%)";
+
+      document.getElementById("fireworks").style.display = "block";
+      document.getElementById("drag-note").style.display = "block";
+
+      document.getElementById("heart-img").style.display = "none";
+      document.getElementById("cheerup-note").style.display = "none";
+      document.getElementById("insta").style.display = "none";
+      document.getElementById("cam").style.display = "none";
+      document.getElementById("camButton").style.display = "none";
+      document.getElementById("camPhoto").style.display = "none";
+      document.getElementById("cheerup-note").style.display = "none";
+      document.getElementById("link").style.display = "block";
+
+      document.getElementById("html").style.backgroundSize = "0px";
+      document.body.style.backgroundSize = "0px";
+
+      scrollToTop();
     }
 
     if ("${text}" == "missing you") {
-      document.getElementById("text").innerHTML = "Then Call Me!";
-      document.getElementById("avatar").src = images[4];
 
+      document.getElementById("text").innerHTML = "Then Call Me!";
+      document.getElementById("avatar").src = avatar[4];
+      document.getElementById("avatar").style.transform = "translateY(30%)";
+
+      document.getElementById("link").style.display  ="block";
+      document.getElementById("link").innerHTML = "📞";
+      document.getElementById("link").href = "https://www.messenger.com/";
+
+      document.getElementById("heart-img").style.display = "none";
+      document.getElementById("cheerup-note").style.display = "none";
+      document.getElementById("insta").style.display = "none";
+      document.getElementById("cam").style.display = "none";
+      document.getElementById("camButton").style.display = "none";
+      document.getElementById("camPhoto").style.display = "none";
+      document.getElementById("cam-note").style.display = "none";
+      document.getElementById("fireworks").style.display = "none";
+      document.getElementById("drag-note").style.display = "none";
+
+      document.getElementById("html").style.backgroundSize = "0px";
+      document.body.style.backgroundSize = "0px";
+
+      scrollToTop();
     }
 
-  //   var h = document.createElement("H1"); // Create the H1 element
-  // if ("${text}" == "easter") {
-  //     var t = document.createTextNode("HIII"); // Create a text element
-  // }
-  // h.appendChild(t); // Append the text node to the H1 element
-  // document.body.insertBefore(h, document.body.childNodes[0]);
-  // h.setAttribute("id", "header");
-  // document.getElementById("header").style.color = "violet";
-  // document.getElementById("header").style.padding = "2em";
-  //
-  // var p_tags=document.getElementsByTagName('p');
-  // for(i=0;i<p_tags.length;i++)
-  // {
-  //     p_tags[i].setAttribute("style","font-size:"+40+"px");
-  // }
-  //
+    function scrollToTop() {
+          window.scrollTo(0, 0);
+      }
 
+    function videoOn() {
+      var video = document.querySelector("#cam");
+      var camPhoto = document.getElementById("camPhoto");
+      var context = camPhoto.getContext("2d");
+
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+          .then(function (stream) {
+            video.srcObject = stream;
+            video.play();
+          })
+          .catch(function (err0r) {
+            console.log("Something went wrong!");
+          });
+      }
+
+      document.getElementById("camButton").addEventListener("click", function() {
+  	  context.drawImage(video, 0, 0, 500, 375);
+});
+}
      `
-
   });
 });
-//
